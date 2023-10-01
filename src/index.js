156d27da493e8fedae3e1a53c4162c2d39ec6426
Arouj <arouj26@gmail.com>
2023-10-01 15:28:29 +0300

    second

--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Navbar from './admin/Components/Navbar';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <div class="container-fluid position-relative d-flex p-0">
+      <div class="content">
+        <Navbar />
+      </div>
+    </div>
   </React.StrictMode>
 );
 
